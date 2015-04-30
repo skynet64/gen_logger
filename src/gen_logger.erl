c1b2ab32dc23dd2304ea96eba32e4589c81afbb0
Feng Lee <ery.lee@gmail.com>
2015-04-30 15:58:59 +0800

    1.0.1

--- src/gen_logger.erl
+++ src/gen_logger.erl
@@ -1,31 +1,31 @@
-%%------------------------------------------------------------------------------
-%% Copyright (c) 2015, Feng Lee <feng@emqtt.io>
-%% 
-%% Permission is hereby granted, free of charge, to any person obtaining a copy
-%% of this software and associated documentation files (the "Software"), to deal
-%% in the Software without restriction, including without limitation the rights
-%% to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-%% copies of the Software, and to permit persons to whom the Software is
-%% furnished to do so, subject to the following conditions:
-%% 
-%% The above copyright notice and this permission notice shall be included in all
-%% copies or substantial portions of the Software.
-%% 
-%% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-%% IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-%% FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-%% AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-%% LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-%% OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-%% SOFTWARE.
-%%------------------------------------------------------------------------------
+%%%-----------------------------------------------------------------------------
+%%% Copyright (c) 2015, Feng Lee <feng@emqtt.io>
+%%%
+%%% Permission is hereby granted, free of charge, to any person obtaining a copy
+%%% of this software and associated documentation files (the "Software"), to deal
+%%% in the Software without restriction, including without limitation the rights
+%%% to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+%%% copies of the Software, and to permit persons to whom the Software is
+%%% furnished to do so, subject to the following conditions:
+%%%
+%%% The above copyright notice and this permission notice shall be included in all
+%%% copies or substantial portions of the Software.
+%%%
+%%% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+%%% IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+%%% FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+%%% AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+%%% LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+%%% OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+%%% SOFTWARE.
+%%%-----------------------------------------------------------------------------
 
 -module(gen_logger).
 
 -include("gen_logger.hrl").
 
 %%%=========================================================================
-%%%  API
+%%% API
 %%%=========================================================================
 -export([new/1, new/2,
          debug/2, debug/3,
@@ -35,71 +35,21 @@
          critical/2, critical/3]).
 
 %%%=========================================================================
-%%%  Behaviour
-%%%=========================================================================
-
--ifdef(use_specs).
-
-%%%=========================================================================
-%%%  API Spec
+%%% Types
 %%%=========================================================================
 
 -type level() :: all | debug | info | warning | error | critical | none.
 
 -type logmod() :: {gen_logger, module(), level()}.
 
--export_type([logmod/0]).
+-export_type([level/0, logmod/0]).
 
--spec new(LevelOrTuple) -> logmod() when
-      LevelOrTuple  :: level() | {atom(), level()}.
-
--spec new(Name, Level) -> logmod() when
-      Name  :: atom(),
-      Level :: level().
-
--spec debug(Msg, Mod) -> ok when
-      Msg :: string(),
-      Mod :: logmod().
--spec debug(Format, Args, Mod) -> ok when
-      Format :: string(),
-      Args   :: list(),
-      Mod    :: logmod().
-
--spec info(Msg, Mod) -> ok when
-      Msg :: string(),
-      Mod :: logmod().
--spec info(Format, Args, Mod) -> ok when
-      Format :: string(),
-      Args :: list(),
-      Mod :: logmod().
-
--spec warning(Msg, Mod) -> ok when
-      Msg :: string(),
-      Mod :: logmod().
--spec warning(Format, Args, Mod) -> ok when
-      Format :: string(),
-      Args :: list(),
-      Mod :: logmod().
-
--spec error(Msg, Mod) -> ok when
-      Msg :: string(),
-      Mod :: logmod().
--spec error(Format, Args, Mod) -> ok when
-      Format :: string(),
-      Args :: list(),
-      Mod :: logmod().
-
--spec critical(Msg, Mod) -> ok when
-      Msg :: string(),
-      Mod :: logmod().
--spec critical(Format, Args, Mod) -> ok when
-      Format:: string(),
-      Args :: list(),
-      Mod :: logmod().
 
 %%%=========================================================================
-%%%  Callbacks
+%%% Behaviour Callbacks
 %%%=========================================================================
+
+-ifdef(use_specs).
 
 -callback debug(Msg :: string()) -> ok. 
 -callback debug(Format :: string(), Args :: list()) -> ok. 
@@ -132,15 +82,19 @@ behaviour_info(_Other) ->
 -endif.
 
 %%%=========================================================================
-%%%  gen_logger API
+%%% gen_logger API
 %%%=========================================================================
 
+-spec new(LevelOrTuple) -> logmod() when
+      LevelOrTuple  :: level() | {atom(), level()}.
 new(Level) when is_atom(Level) ->
     new(console, Level);
-
 new({Name, Level}) ->
     new(Name, Level).
 
+-spec new(Name, Level) -> logmod() when
+      Name  :: atom(),
+      Level :: level().
 new(Name, Level) when is_atom(Name) and is_atom(Level) ->
     {?MODULE, mod(Name), ?LOG_LEVEL_NUM(Level)}.
 
@@ -150,10 +104,18 @@ mod(Name) ->
 %%%-------------------------------------------------------------------------
 %%% DEBUG
 %%%-------------------------------------------------------------------------
+-spec debug(Msg, Mod) -> ok when
+      Msg :: string(),
+      Mod :: logmod().
 debug(Msg, {?MODULE, Logger, Level}) when Level =< ?LOG_DEBUG ->
     Logger:debug(Msg);
 debug(_Msg, {?MODULE, _Logger, _Level}) ->
     ok.
+
+-spec debug(Format, Args, Mod) -> ok when
+      Format :: string(),
+      Args   :: list(),
+      Mod    :: logmod().
 debug(Format, Args, {?MODULE, Logger, Level}) when Level =< ?LOG_DEBUG ->
     Logger:debug(Format, Args);
 debug(_Format, _Args, {?MODULE, _Logger, _Level}) ->
@@ -162,11 +124,18 @@ debug(_Format, _Args, {?MODULE, _Logger, _Level}) ->
 %%%-------------------------------------------------------------------------
 %%% INFO
 %%%-------------------------------------------------------------------------
+-spec info(Msg, Mod) -> ok when
+      Msg :: string(),
+      Mod :: logmod().
 info(Msg, {?MODULE, Logger, Level}) when Level =< ?LOG_INFO ->
     Logger:info(Msg);
 info(_Msg, {?MODULE, _Logger, _Level}) ->
     ok.
 
+-spec info(Format, Args, Mod) -> ok when
+      Format :: string(),
+      Args :: list(),
+      Mod :: logmod().
 info(Format, Args, {?MODULE, Logger, Level}) when Level =< ?LOG_INFO ->
     Logger:info(Format, Args);
 info(_Format, _Args, {?MODULE, _Logger, _Level}) ->
@@ -175,11 +144,18 @@ info(_Format, _Args, {?MODULE, _Logger, _Level}) ->
 %%%-------------------------------------------------------------------------
 %%% WARNING
 %%%-------------------------------------------------------------------------
+-spec warning(Msg, Mod) -> ok when
+      Msg :: string(),
+      Mod :: logmod().
 warning(Msg, {?MODULE, Logger, Level}) when Level =< ?LOG_WARNING ->
     Logger:warning(Msg);
 warning(_Msg, {?MODULE, _Logger, _Level}) ->
     ok.
 
+-spec warning(Format, Args, Mod) -> ok when
+      Format :: string(),
+      Args :: list(),
+      Mod :: logmod().
 warning(Format, Args, {?MODULE, Logger, Level}) when Level =< ?LOG_WARNING ->
     Logger:warning(Format, Args);
 warning(_Format, _Args, {?MODULE, _Logger, _Level}) ->
@@ -188,11 +164,18 @@ warning(_Format, _Args, {?MODULE, _Logger, _Level}) ->
 %%%-------------------------------------------------------------------------
 %%% ERROR
 %%%-------------------------------------------------------------------------
+-spec error(Msg, Mod) -> ok when
+      Msg :: string(),
+      Mod :: logmod().
 error(Msg, {?MODULE, Logger, Level}) when Level =< ?LOG_ERROR ->
     Logger:error(Msg);
 error(_Msg, {?MODULE, _Logger, _Level}) ->
     ok.
 
+-spec error(Format, Args, Mod) -> ok when
+      Format :: string(),
+      Args :: list(),
+      Mod :: logmod().
 error(Format, Args, {?MODULE, Logger, Level}) when Level =< ?LOG_ERROR ->
     Logger:error(Format, Args);
 error(_Format, _Args, {?MODULE, _Logger, _Level}) ->
@@ -201,11 +184,18 @@ error(_Format, _Args, {?MODULE, _Logger, _Level}) ->
 %%%-------------------------------------------------------------------------
 %%% CRITICAL
 %%%-------------------------------------------------------------------------
+-spec critical(Msg, Mod) -> ok when
+      Msg :: string(),
+      Mod :: logmod().
 critical(Msg, {?MODULE, Logger, Level}) when Level =< ?LOG_CRITICAL ->
     Logger:critical(Msg);
 critical(_Msg, {?MODULE, _Logger, _Level}) ->
     ok.
 
+-spec critical(Format, Args, Mod) -> ok when
+      Format:: string(),
+      Args :: list(),
+      Mod :: logmod().
 critical(Format, Args, {?MODULE, Logger, Level}) when Level =< ?LOG_CRITICAL ->
     Logger:critical(Format, Args);
 critical(_Format, _Args, {?MODULE, _Logger, _Level}) ->
